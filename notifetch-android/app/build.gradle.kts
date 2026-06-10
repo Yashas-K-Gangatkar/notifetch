33405ae1a29d6d42d18877e955c4c811b9dc6b91
Z User <z@container>
2026-06-10 12:46:44 +0000

    0cd16b0b-d0a3-47c0-83ce-ba07ba6ac4ac

--- notifetch-android/app/build.gradle.kts
+++ notifetch-android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.notifetch.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.2.1"
+        versionCode = 13
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
