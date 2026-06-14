1890fdcbf15d37db61a0737646a1397d42319e7e
Z User <z@container>
2026-06-14 00:06:39 +0000

    chore: bump versionCode to 20, versionName to 2.5.1

--- notifetch-android/app/build.gradle.kts
+++ notifetch-android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.notifetch.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.5.0"
+        versionCode = 20
+        versionName = "2.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
