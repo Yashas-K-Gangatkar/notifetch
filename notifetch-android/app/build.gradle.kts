c807282b5ba00e69026bf47038a697246942f47e
Z User <z@container>
2026-06-20 05:49:52 +0000

    v2.9.21-vc47: bump versionCode to 47

--- notifetch-android/app/build.gradle.kts
+++ notifetch-android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.notifetch.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 46
-        versionName = "2.9.20"
+        versionCode = 47
+        versionName = "2.9.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
