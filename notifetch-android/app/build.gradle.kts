16bb626d37f24feeacf30da8bf6135e721b0cd0e
Z User <z@container>
2026-07-12 15:54:02 +0000

    bump: v2.9.78-vc105

--- notifetch-android/app/build.gradle.kts
+++ notifetch-android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.notifetch.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 103
-        versionName = "2.9.77"
+        versionCode = 105
+        versionName = "2.9.78"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
